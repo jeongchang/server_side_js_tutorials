e1ec5c0d5ad2077057e78abae6dae440141fdefd
zpro <zepiro00@gmail.com>
2021-12-09 09:08:15 +0900

    end 84/133

--- app_sessiion_file.js
+++ app_sessiion_file.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended:false}));
@@ -7,10 +8,11 @@ app.use(session({
     secret: 'asdfqwer',
     resave : false, // session id를 접속할때마다 새롭게 발급하지 말아라
     saveUninitialized : true // session id를 session을 실제로 사용할 때까지는 발급하지 말아라
+    , store : new FileStore()
 }));
 app.get('/count', function(req,res){
     if(req.session.count){
-        req.session.count++;    
+        req.session.count++;
     }else{
         req.session.count = 1;    
     }
@@ -26,16 +28,22 @@ app.post('/auth/login', function(req, res){
     var pwd = req.body.password;
     if(uname === user.username && pwd === user.password){
         req.session.displayname = user.displayname;
-        res.redirect('/welcome');
+        req.session.save(function(){
+            res.redirect('/welcome');
+        })
     }else{
         res.send('Who are you? <a href="/auth/login">login</a>');
     }
 });
 app.get('/auth/logout', function(req,res){
     delete req.session.displayname;
-    res.sredirect('/welcome');
+    req.session.save(function(){
+        res.redirect('/welcome');
+    })
+    // res.sredirect('/welcome');
 })
 app.get('/welcome', function(req,res){
+    console.log(req.session)
     if(req.session.displayname){
         res.send(`
             <h1>Hello, ${req.session.displayname}</h1>
